ed099ee2e12579d33ed2dd76d77877ba263d5540
insub2004 <insub2004@naver.com>
2023-12-12 21:43:41 +0900

    dd

--- src/main/java/com/nhnacademy/aiot/node/PostgresOutNode.java
+++ src/main/java/com/nhnacademy/aiot/node/PostgresOutNode.java
@@ -9,6 +9,9 @@ import java.sql.Statement;
 import org.json.JSONObject;
 import com.nhnacademy.aiot.message.JsonMessage;
 import com.nhnacademy.aiot.message.Message;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PostgresOutNode extends OutputNode {
     private Connection connect;
     private Statement stmt;
